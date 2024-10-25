8da269c480204bb4c7d6b7ef6a400c5bdf187675
wwelden <wwelden@trinity.edu>
2024-10-25 10:42:53 -0500

    food spawn on click

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -11,6 +11,22 @@ public class GameState : MonoBehaviour
     {
         this.score += score;
     }
+    public GameObject foodPrefab;
 
-    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnFood();
+        }
+    }
+
+    void SpawnFood()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 10.0f; // Set this to be the distance from the camera
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Instantiate(foodPrefab, worldPosition, Quaternion.identity);
+    }
+
 }
