ba6210a4c3099fae75e6523c360be4158114137e
wwelden <wwelden@trinity.edu>
2024-12-15 00:31:29 -0600

    start of salad chop

---
+++ Assets/Scripts/SaladGame/chopped.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class chopped : MonoBehaviour
+{
+    public GameObject rightHalf;
+    public GameObject leftHalf;
+
+    void Start()
+    {
+
+    }
+}
